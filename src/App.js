e9568a4a3551efaf35fc9a4fb95eccb3322cf471
Jameson Chen <[jamesonychan@gmail.com]>
2022-06-09 20:39:22 -0400

    class components commit

--- src/App.js
+++ src/App.js
@@ -1,92 +1,96 @@
-import { useState } from "react";
-import "./css/album.css";
-import "./css/navbar.css";
-import "./css/heading.css";
-import "./css/loader.css";
+import React, { Component } from "react";
 
-function App() {
-  const [albumList, setAlbumList] = useState([]);
-  const [albumNum, setAlbumNum] = useState(4);
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { albumList: [], albumNum: 4 };
+  }
 
-  const fetchApi = async (searchTerm) => {
+  fetchApi = async (searchTerm) => {
     await fetch(
       `https://itunes.apple.com/search?term=${searchTerm}&media=music&entity=album&attribute=artistTerm&limit=200`
     )
       .then((res) => res.json())
-      .then((albumObj) => setAlbumList(albumObj.results));
+      .then((albumObj) => this.setState({ albumList: albumObj.results }));
   };
 
-  const handleInput = (e) => {
+  handleInput = (e) => {
     if (e.code === "Enter") {
-      fetchApi(e.target.value);
-      setAlbumNum(4);
+      // fetchApi
+      this.fetchApi(e.target.value);
+      this.setState({ albumNum: 4 });
     }
   };
 
-  const handleOnClick = () => {
-    if (albumNum > albumList.length) {
+  handleOnClick = () => {
+    if (this.state.albumNum > this.state.albumList.length) {
       alert("already reach max of results");
     }
-    setAlbumNum((albumNum) => albumNum + 4);
+    this.setState((prevState) => ({ albumNum: prevState.albumNum + 4 }));
   };
 
-  return (
-    <>
-      {/* navbar */}
-      <nav className="navbar">
-        <div className="navbar_search-container">
-          <input
-            type="text"
-            className="navbar_searchbar"
-            id="navbar_searchbar"
-            placeholder="Search..."
-            required
-            onKeyPress={handleInput}
-          />
-          <button id="navbar_button">
-            <img
-              className="navbar_search-icon"
-              src="https://img.icons8.com/material-outlined/24/000000/search--v1.png"
-              alt=""
+  render() {
+    const { albumList, albumNum } = this.state;
+    const title =
+      albumList.length > 0
+        ? `${albumList.slice(0, albumNum).length}/${albumList.length} results`
+        : "Search Albums by Artist Name";
+
+    const renderedAlbumList = albumList
+      .slice(0, albumNum)
+      .map((album, index) => (
+        <li key={index} className="album-item">
+          <div className="album-item_container">
+            <img src={album.artworkUrl100} alt="" />
+            <p>{album.collectionCensoredName}</p>
+          </div>
+        </li>
+      ));
+    return (
+      <>
+        {/* navbar */}
+        <nav className="navbar">
+          <div className="navbar_search-container">
+            <input
+              type="text"
+              className="navbar_searchbar"
+              id="navbar_searchbar"
+              placeholder="Search..."
+              required
+              onKeyPress={this.handleInput}
             />
-          </button>
-        </div>
-      </nav>
+            <button id="navbar_button">
+              <img
+                className="navbar_search-icon"
+                src="https://img.icons8.com/material-outlined/24/000000/search--v1.png"
+                alt=""
+              />
+            </button>
+          </div>
+        </nav>
 
-      {/* header */}
-      <header className="heading">
-        <div className="" id="heading_loader"></div>
-        <h1 className="heading_title" id="heading_title">
-          {albumList.length > 0
-            ? `${albumList.slice(0, albumNum).length}/${
-                albumList.length
-              } results`
-            : "Search Albums by Artist Name"}
-        </h1>
-      </header>
+        {/* header */}
+        <header className="heading">
+          <div className="" id="heading_loader"></div>
+          <h1 className="heading_title" id="heading_title">
+            {title}
+          </h1>
+        </header>
 
-      {/* album */}
-      <main className="album">
-        <ul className="album-list" id="album-list">
-          {/* map the album list */}
-          {albumList.slice(0, albumNum).map((album, index) => (
-            <li key={index} className="album-item">
-              <div className="album-item_container">
-                <img src={album.artworkUrl100} alt="" />
-                <p>{album.collectionCensoredName}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
+        {/* album */}
+        <main className="album">
+          <ul className="album-list" id="album-list">
+            {/* map the album list */}
+            {renderedAlbumList}
+          </ul>
 
-        <div className="button_container">
-          <button className="button_loadmore" onClick={handleOnClick}>
-            Load more
-          </button>
-        </div>
-      </main>
-    </>
-  );
+          <div className="button_container">
+            <button className="button_loadmore" onClick={this.handleOnClick}>
+              Load more
+            </button>
+          </div>
+        </main>
+      </>
+    );
+  }
 }
-
-export default App;
